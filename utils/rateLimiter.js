95d7589cac6f51c644a597aef0ec308270c6efb3
Toba <riemehasan06@gmail.com>
2026-07-24 20:55:17 +0600

    remove comments from rateLimiter.js

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,12 +1,5 @@
 const { rateLimit, MINUTE } = require("express-rate-limit");
 
-// const limiter = rateLimit({
-//   windowMs: 15 * MINUTE,
-//   limit: 100,
-//   standardHeaders: "draft-8",
-//   legacyHeaders: false,
-//   ipv6Subnet: 56,
-// });
 
 const createLimiter = ({ windowMs, limit, message }) => {
   return rateLimit({
